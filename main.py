11dc6861c2e7facd90d12d3fd02fd13353f6abc5
morhaf12345 <136889508+morhaf12345@users.noreply.github.com>
2023-07-08 01:58:41 -0700

    n about index and slice for string

---
+++ main.py
@@ -0,0 +1,20 @@
+var1= "valeu"
+# print(var1[0])
+# print(var1[1:])
+str1='hello'
+str2=str1[0:2]+"mm"+str1[4:]
+print(str2)
+
+print(var1)
+
+num1=1
+print(num1)
+print(type(num1))
+
+num2=2.5
+print(num2)
+print(type(num2))
+
+comp =complex(4 + 6j)
+print(comp)
+
